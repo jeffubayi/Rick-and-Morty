b49f881224b48cef553d9e0b22a8570f275e474b
jeffubayi <jeffubayi@gmail.com>
2021-08-23 22:30:08 +0300

    add state to update the query

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
   return (
-    <div>
+    <div className='container'>
     </div>
   );
 }
