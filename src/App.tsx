7a8e4acc60e65ec4ad044b9905a5264c9adbcb48
jeffubayi <jeffubayi@gmail.com>
2021-08-23 22:33:37 +0300

    makes a query to the rest api using axios and update the current state of the query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React,{useState}from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [query, setQuery] = useState('')
+
+  const fetchItems = async () => {
+    setIsLoading(true)
+    const result = await axios(
+      `https://rickandmortyapi.com/api/character?name=${query}`
+    )
+    console.log(result.data.results)
+    setItems(result.data.results)
+    setIsLoading(false)
+  }
   return (
     <div className='container'>
     </div>
