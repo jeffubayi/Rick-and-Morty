8303874b44d10dc1889e98a435c4fb6040f3df76
jeffubayi <jeffubayi@gmail.com>
2021-08-26 08:39:21 +0300

    update characters ui

---
+++ src/components/characters/character.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Character = () => {
+  return (
+    <div className='card'>
+      <div className='card-inner'>
+        <div className='card-front'>
+          <img src=''alt='' />
+        </div>
+        <div className='card-back'>
+          <h1></h1>
+          <ul>
+            <li>
+              <strong>Status:</strong>
+            </li>
+            <li>
+              <strong>Species:</strong> 
+            </li>
+            <li>
+              <strong>Location:</strong> 
+            </li>
+            <li>
+              <strong>Gender:</strong>
+            </li>
+            <li>
+              <strong>Origin:</strong>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Character
