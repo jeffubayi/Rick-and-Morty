ec97aa486c2d4d5d7b5d8177459061a2754d70c1
jeffubayi <jeffubayi@gmail.com>
2021-08-26 08:43:58 +0300

    add props to pass charaters

--- src/components/characters/character.tsx
+++ src/components/characters/character.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
 
-const Character = () => {
+const Character = ({ item }) => {
   return (
     <div className='card'>
       <div className='card-inner'>
         <div className='card-front'>
-          <img src=''alt='' />
+          <img src={item.image} alt='' />
         </div>
         <div className='card-back'>
-          <h1></h1>
+          <h1>{item.name}</h1>
           <ul>
             <li>
-              <strong>Status:</strong>
+              <strong>Status:</strong> {item.status}
             </li>
             <li>
-              <strong>Species:</strong> 
+              <strong>Species:</strong> {item.species}
             </li>
             <li>
-              <strong>Location:</strong> 
+              <strong>Location:</strong> {item.location.name}
             </li>
             <li>
-              <strong>Gender:</strong>
+              <strong>Gender:</strong> {item.gender}
             </li>
             <li>
-              <strong>Origin:</strong>
+              <strong>Origin:</strong> {item.origin.name}
             </li>
           </ul>
         </div>
