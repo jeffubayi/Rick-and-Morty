ebc3186a10617d4a31d228ee833f7ee4826a271a
jeffubayi <jeffubayi@gmail.com>
2021-08-27 17:51:41 +0300

    app - add the charater wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React,{useState}from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import CharacterWrapper from '../src/components/characters/characterWrapper'
+import './App.css'
 
-function App() {
+const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [query, setQuery] = useState('')
 
-  const fetchItems = async () => {
-    setIsLoading(true)
-    const result = await axios(
-      `https://rickandmortyapi.com/api/character?name=${query}`
-    )
-    console.log(result.data.results)
-    setItems(result.data.results)
-    setIsLoading(false)
-  }
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true)
+      const result = await axios(
+        `https://rickandmortyapi.com/api/character?name=${query}`
+      )
+
+      console.log(result.data.results)
+
+      setItems(result.data.results)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+  }, [query])
+
   return (
     <div className='container'>
+      <CharacterWrapper isLoading={isLoading} items={items} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
