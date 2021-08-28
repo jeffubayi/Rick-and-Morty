cd8a32b245d56d72240f3a1ad9a1d8e7ac44617b
jeffubayi <jeffubayi@gmail.com>
2021-08-28 11:08:09 +0300

    pages - create a header to display logo

---
+++ src/components/views/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="center">
+      <img
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/19643055883023.5996f8afa3a8f.gif"
+        alt=""
+        style={{ width: "25rem", height: "10rem" }}
+      />
+    </header>
+  );
+};
+
+export default Header;
